f4c97a69daa76b8648333e55851841f462018e6e
gmahima <mahimagangavarapu@gmail.com>
2020-10-06 17:36:53 +0530

    add framermotion

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,28 @@
 import Head from 'next/head'
 import styled from 'styled-components'
 import tw from 'twin.macro'
-
-const Container = styled.div`
+import {motion} from 'framer-motion'
+const Container = styled(motion.div)`
 ${tw `
- bg-gray-900 min-h-screen
+ bg-gray-100 min-h-screen
+ flex flex-col items-center
+ p-12
+`}
+`
+const Sample = styled(motion.div)`
+${tw `
+ bg-gray-400 w-56 h-56
+ text-center 
+ p-12
 `}
 `
 
 export default function Home() {
   return (
     <Container>
-      asdf
+      <Sample animate={{ scale: 0.5 }}>
+        hey
+      </Sample>
     </Container>
   )
 }
